ba5bf4daed3e25e9be4e60bb2b48871f0e590f7f
Jan Simon <simonj@hochschule-trier.de>
2022-01-18 17:34:22 +0100

    Add skeletal animation support

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,7 @@
 #include <glad/gl.h>
 #include <glfw/glfw3.h>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 #include <iostream>
 #include <vector>
@@ -11,16 +12,32 @@
 namespace
 {
 
+// Vertex definition
+struct Vertex
+{
+  glm::vec3 position;
+  glm::ivec4 boneIds;    // Which bones affect this vertex (indices into the bone transforms array)
+  glm::vec4 boneWeights; // How much each indexed bone affects this vertex, elements sum up to 1.0
+};
+
+// Bone definition
+struct Bone
+{
+  glm::mat4 inverseBindMatrix; // Inverse bind pose bone transform (transforms from unposed bone to model space origin)
+  glm::mat4 posedTransform;    // Posed bone transform in bone space (translation * rotation * scale)
+};
+
 // Window constants
+constexpr char windowTitle[] = "Poser";
 constexpr int windowWidth = 640;
 constexpr int windowHeight = 400;
-constexpr char windowTitle[] = "Poser";
+constexpr float windowAspectRatio = static_cast<float>(windowWidth) / static_cast<float>(windowHeight);
 
 // OpenGL constants
 constexpr GLsizei shaderInfoLogLength = 512;
 
 // File constants
-constexpr char modelFileName[] = "models/model.blend";
+constexpr char modelFileName[] = "models/silly_dancing.fbx";
 
 // Color constants
 constexpr glm::vec4 clearColor = { 0.9f, 0.4f, 0.1f, 1.0f };
@@ -28,24 +45,108 @@ constexpr glm::vec4 quadColor = { 0.1f, 0.4f, 0.9f, 1.0f };
 
 // Camera constants
 constexpr float cameraMinDistance = 0.5f;
-constexpr float cameraPositionY = 1.25f;
-constexpr float cameraTargetY = 0.5f;
+constexpr float cameraPositionY = 4.0f;
+constexpr float cameraTargetY = 1.5f;
+constexpr float cameraNear = 0.1f;
+constexpr float cameraFar = 100.0f;
+constexpr float cameraFov = 45.0f; // Vertical field of view in degrees
 
 // Camera variables
 bool mouseDown = false;
 float cameraAngle = glm::radians(45.0f);
 float cameraDistance = 5.0f;
 double lastMouseX;
-glm::mat4 viewMatrix;
 
 // Geometry variables
-std::vector<glm::vec3> vertices;
+std::vector<Vertex> vertices;
 std::vector<unsigned int> indices;
+
+// Animation variables
+Assimp::Importer importer;
+std::vector<Bone> bones;
+std::vector<glm::mat4> boneTransforms; // Transforms from unposed to posed bone in model space
+unsigned int frameIndex = 0u;          // Current animation frame
+
+glm::mat4 assimpToGlmMat4(const aiMatrix4x4& m)
+{
+  return glm::transpose(glm::make_mat4(&m.a1)); // Convert row-major (assimp) to column-major (glm)
+}
+
+int findNamedBone(const aiString& name)
+{
+  const aiMesh* mesh = importer.GetScene()->mMeshes[0];
+  for (unsigned int i = 0; i < mesh->mNumBones; ++i)
+  {
+    if (name == mesh->mBones[i]->mName)
+    {
+      return static_cast<int>(i);
+    }
+  }
+  return -1;
+}
+
+void processNode(const aiNode* node, const glm::mat4& parentTransform)
+{
+  glm::mat4 globalTransform = parentTransform;
+
+  // Check if this node is a bone
+  const int boneIndex = findNamedBone(node->mName);
+  if (boneIndex > -1)
+  {
+    const Bone& bone = bones.at(static_cast<size_t>(boneIndex));
+
+    // Posed bone transform in model space (transforms from the model space origin to the posed bone)
+    globalTransform *= bone.posedTransform;
+
+    // Store the transform from the unposed to the posed bone in model space by transforming from the unposed bone to
+    // the model space origin (through the inverse bind matrix) and then from there to the posed bone in model space
+    boneTransforms.at(static_cast<size_t>(boneIndex)) = globalTransform * bone.inverseBindMatrix;
+  }
+
+  // Process the children of this node recursively
+  for (unsigned int i = 0u; i < node->mNumChildren; ++i)
+  {
+    processNode(node->mChildren[i], globalTransform);
+  }
+}
+
+void updateAnimation()
+{
+  const aiScene* scene = importer.GetScene();
+  const aiAnimation* animation = scene->mAnimations[0];
+
+  // Load the local bone transforms for the current animation frame
+  for (unsigned int i = 0u; i < animation->mNumChannels; ++i)
+  {
+    const aiNodeAnim* channel = animation->mChannels[i];
+
+    // Skip channels that do not represent a bone
+    const int boneIndex = findNamedBone(channel->mNodeName);
+    if (boneIndex < 0)
+    {
+      continue;
+    }
+
+    const aiVector3D& translation = channel->mPositionKeys[frameIndex % channel->mNumPositionKeys].mValue;
+    const aiQuaternion& rotation = channel->mRotationKeys[frameIndex % channel->mNumRotationKeys].mValue;
+    const aiVector3D& scale = channel->mScalingKeys[frameIndex % channel->mNumScalingKeys].mValue;
+
+    const glm::mat4 t = glm::translate(glm::mat4(1.0f), glm::vec3(translation.x, translation.y, translation.z));
+    const glm::mat4 r = glm::toMat4(glm::quat(rotation.w, rotation.x, rotation.y, rotation.z));
+    const glm::mat4 s = glm::scale(glm::mat4(1.0f), glm::vec3(scale.x, scale.y, scale.z));
+
+    // Store the new transform of the posed bone in bone space
+    bones.at(boneIndex).posedTransform = t * r * s;
+  }
+
+  // Re-process the entire skeletal hierarchy, starting from the root node
+  processNode(scene->mRootNode, glm::mat4(1.0f));
+}
 
 void cursorPositionCallback(GLFWwindow* window, double x, double y)
 {
-  if (mouseDown)
   // Tumble the camera
+  if (mouseDown)
   {
     const double deltaX = x - lastMouseX;
     cameraAngle -= (static_cast<float>(deltaX * glm::pi<double>()) / windowWidth);
@@ -55,8 +156,8 @@ void cursorPositionCallback(GLFWwindow* window, double x, double y)
 
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 {
-  if (button == GLFW_MOUSE_BUTTON_LEFT)
   // Store mouse button state
+  if (button == GLFW_MOUSE_BUTTON_LEFT)
   {
     mouseDown = (action == GLFW_PRESS);
   }
@@ -113,10 +214,8 @@ int main()
 
   // Load a model
   {
-    Assimp::Importer importer;
-
     // Parse the file
-    const aiScene* scene = importer.ReadFile(modelFileName, aiProcess_Triangulate);
+    const aiScene* scene = importer.ReadFile(modelFileName, aiProcess_Triangulate | aiProcess_JoinIdenticalVertices);
     if (!scene)
     {
       std::cerr << "Failed to load model:\n" << importer.GetErrorString();
@@ -124,18 +223,10 @@ int main()
       return EXIT_FAILURE;
     }
 
-    if (scene->mNumMeshes > 0)
     // Load the first mesh if there is one
+    if (scene->mNumMeshes > 0)
     {
       const aiMesh* mesh = scene->mMeshes[0];
-
-      // Load the vertices
-      vertices.resize(static_cast<size_t>(mesh->mNumVertices));
-      for (unsigned int i = 0u; i < mesh->mNumVertices; ++i)
-      {
-        const aiVector3D& vertex = mesh->mVertices[i];
-        vertices.at(i) = glm::vec3(vertex.x, vertex.y, vertex.z);
-      }
 
       // Load the indices
       indices.resize(static_cast<size_t>(mesh->mNumFaces) * 3u);
@@ -147,42 +238,98 @@ int main()
         indices.at(static_cast<size_t>(i) * 3u + 1u) = face.mIndices[1];
         indices.at(static_cast<size_t>(i) * 3u + 2u) = face.mIndices[2];
       }
+
+      // Load the vertices
+      vertices.resize(static_cast<size_t>(mesh->mNumVertices));
+      for (unsigned int i = 0u; i < mesh->mNumVertices; ++i)
+      {
+        const aiVector3D& vertex = mesh->mVertices[i];
+        vertices.at(i).position = glm::vec3(vertex.x, vertex.y, vertex.z);
+
+        // These will be set in the next step
+        vertices.at(i).boneIds = glm::ivec4(-1);
+        vertices.at(i).boneWeights = glm::vec4(0.0f);
+      }
+
+      // Load the bones
+      bones.resize(mesh->mNumBones);
+      boneTransforms.resize(mesh->mNumBones);
+      for (unsigned int i = 0u; i < mesh->mNumBones; ++i)
+      {
+        const aiBone* boneInfo = mesh->mBones[i];
+
+        // Store the inverse bind matrix for this bone
+        bones.at(i).inverseBindMatrix = assimpToGlmMat4(boneInfo->mOffsetMatrix);
+
+        // Iterate through all the vertices that this bone affects
+        for (unsigned int j = 0u; j < boneInfo->mNumWeights; ++j)
+        {
+          const aiVertexWeight& weight = boneInfo->mWeights[j];
+          Vertex& affectedVertex = vertices.at(weight.mVertexId);
+
+          // Find the first unpopulated element for this vertex
+          unsigned int element = 0u;
+          while (element < 3u)
+          {
+            if (affectedVertex.boneIds[element] < 0)
+            {
+              break;
+            }
+            ++element;
+          }
+
+          // Mark this vertex as being affected by the bone
+          affectedVertex.boneIds[element] = i;
+          affectedVertex.boneWeights[element] = weight.mWeight;
+        }
+      }
     }
   }
 
-  // Set up some geometry
+  // Set up geometry
   {
-    // Set up a vertex array to capture the following vertex and index buffer
+    // Generate and bind a vertex array to capture the following vertex and index buffer
     {
       GLuint vertexArray;
       glGenVertexArrays(1, &vertexArray);
       glBindVertexArray(vertexArray);
     }
 
-    // Set up a vertex buffer
+    // Generate and fill an index buffer
     {
-      GLuint vertexBuffer;
-      glGenBuffers(1, &vertexBuffer);
-      glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-      glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(sizeof(glm::vec3) * vertices.size()), vertices.data(),
-                   GL_STATIC_DRAW);
-
-      glEnableVertexAttribArray(0);
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, reinterpret_cast<void*>(sizeof(float) * 0));
-    }
-
-    // Set up an index buffer
-    GLuint indexBuffer;
-    {
+      GLuint indexBuffer;
       glGenBuffers(1, &indexBuffer);
       glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
       glBufferData(GL_ELEMENT_ARRAY_BUFFER, static_cast<GLsizeiptr>(sizeof(unsigned int) * indices.size()),
                    indices.data(), GL_STATIC_DRAW);
     }
+
+    // Generate and fill a vertex buffer
+    {
+      GLuint vertexBuffer;
+      glGenBuffers(1, &vertexBuffer);
+      glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+      glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(sizeof(Vertex) * vertices.size()), vertices.data(),
+                   GL_STATIC_DRAW);
+    }
+
+    // Apply the vertex definition
+    {
+      glEnableVertexAttribArray(0);
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                            reinterpret_cast<void*>(offsetof(Vertex, position)));
+
+      glEnableVertexAttribArray(1);
+      glVertexAttribIPointer(1, 4, GL_INT, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex, boneIds)));
+
+      glEnableVertexAttribArray(2);
+      glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+                            reinterpret_cast<void*>(offsetof(Vertex, boneWeights)));
+    }
   }
 
   // Set up a shader program
-  GLint viewUniformLocation;
+  GLint viewUniformLocation, boneTransformsUniformLocation;
   {
     // Compile the vertex shader
     GLuint vertexShader;
@@ -192,8 +339,19 @@ int main()
       const GLchar* source = R"(#version 330 core
                                 uniform mat4 view;
                                 uniform mat4 projection;
-                                in vec3 inPosition;
-                                void main() { gl_Position = projection * view * vec4(inPosition, 1.0); })";
+                                uniform mat4 boneTransforms[64];
+                                layout(location = 0) in vec3 inPosition;
+                                layout(location = 1) in ivec4 inBoneIds;
+                                layout(location = 2) in vec4 inBoneWeights;
+                                void main()
+                                {
+                                  mat4 boneTransform = mat4(0.0);
+                                  for (int i = 0; i < 4; ++i)
+                                  {
+                                    boneTransform += boneTransforms[inBoneIds[i]] * inBoneWeights[i];
+                                  }
+                                  gl_Position = projection * view * boneTransform * vec4(inPosition, 1.0);
+                                })";
 
       glShaderSource(vertexShader, 1, &source, nullptr);
       glCompileShader(vertexShader);
@@ -260,7 +418,7 @@ int main()
       glDeleteShader(fragmentShader);
     }
 
-    // Use shader program and set uniform values
+    // Use shader program, retrieve uniform locations and set constant uniform values
     {
       glUseProgram(program);
 
@@ -270,6 +428,17 @@ int main()
         if (viewUniformLocation < 0)
         {
           std::cerr << "Failed to get view matrix uniform location";
+          glfwTerminate();
+          return EXIT_FAILURE;
+        }
+      }
+
+      // Retrieve bone transforms location
+      {
+        boneTransformsUniformLocation = glGetUniformLocation(program, "boneTransforms");
+        if (boneTransformsUniformLocation < 0)
+        {
+          std::cerr << "Failed to get bone transforms uniform location";
           glfwTerminate();
           return EXIT_FAILURE;
         }
@@ -285,8 +454,7 @@ int main()
           return EXIT_FAILURE;
         }
 
-        const glm::mat4 projectionMatrix =
-          glm::perspective(45.0f, static_cast<float>(windowWidth) / static_cast<float>(windowHeight), 0.1f, 100.0f);
+        const glm::mat4 projectionMatrix = glm::perspective(cameraFov, windowAspectRatio, cameraNear, cameraFar);
         glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
       }
 
@@ -306,20 +474,30 @@ int main()
     }
   }
 
-  while (!glfwWindowShouldClose(window))
   // Main loop
+  while (!glfwWindowShouldClose(window))
   {
+    // Update
+    {
+      ++frameIndex;
+      updateAnimation();
+    }
+
     // Render
     {
       glClear(GL_COLOR_BUFFER_BIT);
 
-      // Set view matrix
+      // Set view matrix uniform
       {
-        viewMatrix = glm::lookAt(glm::vec3(glm::sin(cameraAngle) * cameraDistance, cameraPositionY,
-                                           glm::cos(cameraAngle) * cameraDistance),
-                                 glm::vec3(0.0f, cameraTargetY, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 viewMatrix = glm::lookAt(glm::vec3(glm::sin(cameraAngle) * cameraDistance, cameraPositionY,
+                                                     glm::cos(cameraAngle) * cameraDistance),
+                                           glm::vec3(0.0f, cameraTargetY, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         glUniformMatrix4fv(viewUniformLocation, 1, GL_FALSE, glm::value_ptr(viewMatrix));
       }
+
+      // Set bone transforms uniform
+      glUniformMatrix4fv(boneTransformsUniformLocation, static_cast<GLsizei>(boneTransforms.size()), GL_FALSE,
+                         glm::value_ptr(boneTransforms[0]));
 
       glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(indices.size()), GL_UNSIGNED_INT, 0);
 
